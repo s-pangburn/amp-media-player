60132305ed62bc0b6626bec76ad53aea3ad8f7e5
New Ending <New Ending@10.15.172.148>
2014-11-19 01:56:28 -0800

    Finished loadPlaylist method; updated savePlaylist to make the xml file more readable.

--- amp-media-player/src/Playlist.java
+++ amp-media-player/src/Playlist.java
@@ -1,21 +1,28 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.Locale;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
  
-import org.w3c.dom.Attr;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 
@@ -36,27 +43,90 @@ public class Playlist {
 		currentItemIndex = 0;
 		shuffle = false;
 		repeat = false;
-		
-		playlist.add("FirstFile.mp3");
-		playlist.add("SecondFile.wmv");
-		playlist.add("JustForKicks.mp4");
 	
+		playlist.add("first.mp3");
+		playlist.add("second.mp3");
+		playlist.add("third.mp3");
+		playlist.add("fourth.mp3");
 	}
 	
-	/*public void loadPlaylist(String f) {
+	/* INITIAL CODE USING DOM PARSER (DIDN'T WORK, SAVING IN CASE NEEDED)
+	 * public void loadPlaylist(String f) {
+		//TODO: clear playlist initially with confirmation message (after parsing works)
 		try {
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            File file = new File(f);
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			
+			factory.setIgnoringElementContentWhitespace(false);
+			
+			File file = new File(f);
+			Document document = builder.parse(file);
 
-            if(file.exists()) {
-            	Document doc = db.parse(file);
-                Element docEle = doc.getDocumentElement();
-            }
+			NodeList nodeList = document.getDocumentElement().getChildNodes();
+			System.out.println(nodeList.toString());
+
+			for(int i = 0; i < nodeList.getLength(); i++) {
+				System.out.println(nodeList.getLength());
+				Node node = nodeList.item(i);
+				System.out.println(node.toString());
+
+				if(node.getNodeType() == Node.ELEMENT_NODE) {
+					Element elem = (Element) node;
+					System.out.println(elem.toString()); //Debug println
+					String mediaFile = elem.getElementsByTagName("Name").item(i).getChildNodes().item(0).getNodeValue();
+					System.out.println("I added a file!"); //Debug println
+					playlist.add(mediaFile);
+
+				}
+			}
+
+			System.out.println(playlist);
+
+
+		} catch (Exception e) {
+			System.out.println(e);
 		}
 
 	}*/
+	
 
+	public void loadPlaylist(String f) {
+		//Clear Playlist
+		playlist.clear();
+		
+		//TODO: Add confirmation message (in GUI?) saying that playlist is empty. (placeholders below)
+		System.out.println("Playlist: " + playlist);
+		System.out.println("The playlist has been cleared! Loading playlist...");
+		
+		try {
+			//Opening file
+			Scanner sc = new Scanner(new BufferedReader(new FileReader(f)));
+			sc.useLocale(Locale.US);
+
+			//Checks whether or not there is another line inside the xml file
+			while (sc.hasNext()) {
+				//Sets the string equal to anything inside the Media tags
+				String regex = "(\\s)*<Media>(.*)</Media>";
+				
+				Pattern p = Pattern.compile(regex);
+				String check = sc.nextLine();
+				Matcher m = p.matcher(check);
+				
+				//If it finds <Media>text here</Media>, it will add it to the playlist Arraylist.
+				if (m.matches()) {
+					playlist.add(m.group(2));
+				}
+			}
+
+		} catch (NullPointerException e) {
+			System.out.println("Error!");
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+		}
+
+		System.out.println(playlist);
+	}
+    
 	public void savePlaylist() {
 		try {
 
@@ -67,54 +137,30 @@ public class Playlist {
 			Document doc = docBuilder.newDocument();
 			Element rootElement = doc.createElement("Playlist");
 			doc.appendChild(rootElement);
-
-			// staff elements
-			Element songPath = doc.createElement("SongPath");
-			rootElement.appendChild(songPath);
-
-			// shorten way
-			// staff.setAttribute("id", "1");
 			
 			String fileName = "";
 			Element song;
 			
+			//Loop amount is equal to the number of files inside the arraylist
 			for(int i = 0; i < playlist.size(); i++) {
+				//Finds the file path from the arraylist
 				fileName = playlist.get(i);
 				
+				//Creates a new Media element and puts the media file into the element
 				song = doc.createElement("Media");
 				song.appendChild(doc.createTextNode(fileName));
-				songPath.appendChild(song);
+				rootElement.appendChild(song);
 			}
 
-			/* firstname elements
-			Element firstname = doc.createElement("firstname");
-			firstname.appendChild(doc.createTextNode("yong"));
-			songPath.appendChild(firstname);
-
-			// lastname elements
-			Element lastname = doc.createElement("lastname");
-			lastname.appendChild(doc.createTextNode("mook kim"));
-			songPath.appendChild(lastname);
-
-			// nickname elements
-			Element nickname = doc.createElement("nickname");
-			nickname.appendChild(doc.createTextNode("mkyong"));
-			songPath.appendChild(nickname);
-
-			// salary elements
-			Element salary = doc.createElement("salary");
-			salary.appendChild(doc.createTextNode("100000"));
-			staff.appendChild(salary);
-			*/
 
 			// write the content into xml file
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+			
+			
 			DOMSource source = new DOMSource(doc);
 			StreamResult result = new StreamResult(new File("file.xml"));
-
-			// Output to console for testing
-			// StreamResult result = new StreamResult(System.out);
 
 			transformer.transform(source, result);
 
@@ -145,5 +191,6 @@ public class Playlist {
 		Playlist test = new Playlist();
 		test.init();
 		test.savePlaylist();
+		test.loadPlaylist("file.xml");
 	}
 }
